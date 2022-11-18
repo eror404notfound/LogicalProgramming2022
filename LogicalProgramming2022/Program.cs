3ebf481a743a6929ae7cec45f3ad41103e73998b
Ankit <ankitcool4370@gmail.com>
2022-11-18 21:38:09 +0530

    FibbnocciSeries

---
+++ LogicalProgramming2022/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstNumber = 0;
+            int SecondNumber = 1;
+            int nextNumber;
+            int numberOfElements;
+
+            Console.WriteLine("Enter the number of element : ");
+            numberOfElements = Convert.ToInt32(Console.ReadLine());
+            if (numberOfElements < 2)
+            {
+                Console.WriteLine("Enter the number greater than two : ");
+            }
+            else
+            {
+                Console.WriteLine(firstNumber + " " + SecondNumber + " ");
+                for (int i = 2; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + SecondNumber;
+                    Console.Write(nextNumber + " ");
+                    firstNumber = SecondNumber;
+                    SecondNumber = nextNumber;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
